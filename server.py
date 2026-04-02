2858a63f820c2f3d851efe59e61d9c4c20bc8d7e
sar404 <sarimjurah@gmail.com>
2026-04-02 13:34:44 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,4 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import urllib.parse
-import os
-
-PORT = int(os.environ.get("PORT", 8000))
-
 class OTAHandler(BaseHTTPRequestHandler):
-
-    def do_POST(self):
-        length = int(self.headers.get('Content-Length', 0))
-        body = self.rfile.read(length).decode()
-
-        print("REQ:", body)
-
-        # balas ke endpoint XML lokal
-        xml_url = "http://raw.githubusercontent.com/sar404/otaupdate/refs/heads/main/update.xml"
-
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(f"url={xml_url}".encode())
 
     def do_GET(self):
         if self.path == "/update.xml":
@@ -29,11 +9,9 @@ class OTAHandler(BaseHTTPRequestHandler):
             xml = """<?xml version="1.0" encoding="utf-8"?>
 <update>
     <version>1.0.2</version>
-    <url>http://YOUR-IP/update.zip</url>
+    <url>http://YOUR-SERVER/update.zip</url>
     <md5>xxxxxxxx</md5>
     <size>850000000</size>
 </update>
 """
             self.wfile.write(xml.encode())
-
-HTTPServer(("0.0.0.0", PORT), OTAHandler).serve_forever()
